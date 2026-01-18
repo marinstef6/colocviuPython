d8bde9dc275e827590e5e066924864654732b541
marinstef6 <132059219+marinstef6@users.noreply.github.com>
2026-01-18 13:09:47 +0200

    Add EXTRA_FULL constant for full response

--- app/src/main/java/ro/pub/cs/systems/eim/colocviu_python/Constants.java
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu_python/Constants.java
@@ -3,6 +3,8 @@ public class Constants {
     public static final String TAG = "EIM";
     public static final String ACTION_DICT = "ro.pub.cs.systems.eim.DICT";
     public static final String EXTRA_DEF = "definition";
+    public static final String EXTRA_FULL = "full_response";
+
     public static final String URL_TEMPLATE =
             "https://api.dictionaryapi.dev/api/v2/entries/en/%s";
 }
